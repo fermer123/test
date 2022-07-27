1cd279db4a0acd56344161ef8b55b4a94a10c688
fermer123 <fermer_525@mail.ru>
2022-07-27 16:19:53 +0500

    product? не забыть подключить ссылку на него

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,15 +1,24 @@
+import axios from 'axios';
 import style from './Product.module.scss';
 import '../../index.scss';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Product = () => {
-  const location = useLocation();
   const params = useParams();
-  console.log(params);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    axios(`http://localhost:8080/clothes/${params.id}`).then(({ data }) =>
+      setProduct(data),
+    );
+  }, []);
   return (
     <div className='container'>
-      <h2 className='title'>Product</h2>
-      <div className={style.product}></div>
+      <h2 className={'title' + ' ' + style.title}>Product</h2>
+      <div className={style.way}>Главная - Свитшшоты - {product.title}</div>
+      <div className={style.product}>{product.title}</div>
     </div>
   );
 };
