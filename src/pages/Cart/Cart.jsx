febd4b14daac0a7a3f3d656c979fb8243024a37c
fermer123 <fermer_525@mail.ru>
2022-08-01 16:33:10 +0500

    cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,22 @@
+import { NavLink } from 'react-router-dom';
 import style from './Cart.module.scss';
 
 const Cart = () => {
   return (
     <div className='container'>
-      <h2 className='title'>Cart</h2>
+      <div className={style.cart}>
+        <h2 className='title'>Cart</h2>
+        <div className={style.shop_link}>
+          <NavLink to='/'>Главная</NavLink> - Коризна
+        </div>
+
+        <ul className={style.ol_name}>
+          <li className={style.li_name}>Товар</li>
+          <li className={style.li_name}>Цена</li>
+          <li className={style.li_name}>Количество</li>
+          <li className={style.li_name}>Всего</li>
+        </ul>
+      </div>
     </div>
   );
 };
