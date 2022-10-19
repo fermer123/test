f06d015ac6a7104d6e934b028a1e6b658944198a
fermer123 <fermer_525@mail.ru>
2022-10-19 21:33:27 +0500

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,38 +6,40 @@ import Logo from '../img/Logo.svg';
 const Footer = () => {
   const { t } = useTranslation();
   return (
-    <div className={style.footer}>
-      <ul className={style.list}>
-        <li className={style.footer_logo}>
-          <img className={style.logo} src={Logo} />
-          <span>womazing</span>
-        </li>
-        <li>
-          <NavLink className={style.header_link} to='/'>
-            {t('header.link1')}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={style.header_link} to='/shop'>
-            {t('header.link2')}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={style.header_link} to='/brands'>
-            {t('header.link3')}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={style.header_link} to='/contact'>
-            {t('header.link4')}
-          </NavLink>
-        </li>
-        <li>
-          <div>+7 (495) 823-54-12</div>
-          <div>hello@womazing.com</div>
-        </li>
-      </ul>
-    </div>
+    <footer className={style.footer}>
+      <div className={style.footer_container}>
+        <ul className={style.list}>
+          <li className={style.footer_logo}>
+            <img className={style.logo} src={Logo} />
+            <span>womazing</span>
+          </li>
+          <li>
+            <NavLink className={style.header_link} to='/'>
+              {t('header.link1')}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={style.header_link} to='/shop'>
+              {t('header.link2')}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={style.header_link} to='/brands'>
+              {t('header.link3')}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={style.header_link} to='/contact'>
+              {t('header.link4')}
+            </NavLink>
+          </li>
+          <li>
+            <div>+7 (495) 823-54-12</div>
+            <div>hello@womazing.com</div>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 };
 
